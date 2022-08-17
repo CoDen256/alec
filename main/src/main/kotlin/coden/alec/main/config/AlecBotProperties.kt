0df1e74c98b2266616d0d1c9abb1bcf63623e45d
Denys Chernyshov <den.blackshov@gmail.com>
2022-08-17 13:53:28 +0200

    Fix properties

--- main/src/main/kotlin/coden/alec/main/config/AlecBotProperties.kt
+++ main/src/main/kotlin/coden/alec/main/config/AlecBotProperties.kt
@@ -1,9 +1,8 @@
 package coden.alec.main.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
-@ConstructorBinding
 @ConfigurationProperties("bot")
-data class AlecBotProperties (var token: String) {
+class AlecBotProperties {
+    lateinit var token: String
 }
