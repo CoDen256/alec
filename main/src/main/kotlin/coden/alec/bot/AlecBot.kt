be0d95584a4b9ed18c28e8c95f2f99a8bf063ea8
Denys Chernyshov <den.blackshov@gmail.com>
2022-08-17 15:22:35 +0200

    inline buttons test

--- main/src/main/kotlin/coden/alec/bot/AlecBot.kt
+++ main/src/main/kotlin/coden/alec/bot/AlecBot.kt
@@ -6,7 +6,16 @@ import coden.alec.interactors.definer.scale.CreateScaleInteractor
 import coden.alec.interactors.definer.scale.ListScalesInteractor
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.callbackQuery
 import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.inlineQuery
+import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
+import com.github.kotlintelegrambot.entities.KeyboardReplyMarkup
+import com.github.kotlintelegrambot.entities.inlinequeryresults.InlineQueryResult
+import com.github.kotlintelegrambot.entities.inlinequeryresults.InputMessageContent
+import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
+import com.github.kotlintelegrambot.entities.keyboard.KeyboardButton
 import gateway.memory.ScaleInMemoryGateway
 
 class AlecBot (
@@ -36,9 +45,31 @@ class AlecBot (
                 )
                 CreateScaleController(activator, bot).handle(message, args)
             }
+            command("inlineButtons") {
+                val inlineKeyboardMarkup = InlineKeyboardMarkup.create(
+                    listOf(InlineKeyboardButton.CallbackData(text = "Test Inline Button", callbackData = "testButton")),
+                    listOf(InlineKeyboardButton.CallbackData(text = "Show alert", callbackData = "showAlert"))
+                )
+                bot.sendMessage(
+                    chatId = ChatId.fromId(message.chat.id),
+                    text = "Hello, inline buttons!",
+                    replyMarkup = inlineKeyboardMarkup
+                )
+            }
+
+            callbackQuery("testButton") {
+                val inlineKeyboardMarkup = InlineKeyboardMarkup.create(
+                    listOf(InlineKeyboardButton.CallbackData(text = "Test Inline Button 2", callbackData = "testButton")),
+                    listOf(InlineKeyboardButton.CallbackData(text = "Show alert 2", callbackData = "showAlert"))
+                )
+                val chatId = callbackQuery.message?.chat?.id ?: return@callbackQuery
+                bot.editMessageText(ChatId.fromId(chatId), callbackQuery.message?.messageId, text="selected ${callbackQuery.data}",
+
+                    replyMarkup =inlineKeyboardMarkup )
+            }
+
         }
     }
-
     fun launch(){
         bot.startPolling()
     }
