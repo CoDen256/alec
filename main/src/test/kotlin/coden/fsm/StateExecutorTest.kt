1f0d627874e582cd278e2cf2b0795d17190e5981
Denys Chernyshov <den.blackshov@gmail.com>
2022-10-09 15:01:16 +0200

    ADd tests for state executor

--- main/src/test/kotlin/coden/fsm/StateExecutorTest.kt
+++ main/src/test/kotlin/coden/fsm/StateExecutorTest.kt
@@ -1,126 +1,141 @@
 package coden.fsm
 
 import coden.fsm.Entry.Companion.entry
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class StateExecutorTest {
 
-    val se = StateExecutor()
-
-    init {
-        se.apply {
-            submit(Start)
-            submit(Back)
-
-        }
-    }
     /**
-     * S1 -> (start) -> S2 -> (back) -> S1 -> (back) -> S1
-     * S1 -> (help) -> S3 -> (back) -> S1
-     * S2 -> (start) -> S3 -> (start) -> S1
-     * S3 -> (help) -> S2 -> (help) -> S1
-     * S1 -> (text("1")) -> S1
-     * S1 -> (text("2")) -> S2
-     * S1 -> (text("3")) -> S3
-     * S2 -> (text(any)) -> S1
-     * S3 -> (text(any)) -> S2
-     * S1 -> (text(any)) -> S4
-     * S4 -> any -> S4
+     * A -> (start) -> B -> (back) -> A -> (back) -> A
+     * A -> (help) -> C -> (back) -> A
+     * B -> (start) -> C -> (start) -> A
+     * C -> (help) -> B -> (help) -> A
+     * A -> (text("1")) -> A
+     * A -> (text("2")) -> B
+     * A -> (text("3")) -> C
+     * B -> (text(any)) -> A
+     * C -> (text(any)) -> B
+     * A -> (text(any)) -> D
+     * D -> any -> D
      * */
 
     @Test
     internal fun name() {
 
-        var commands = ""
-        var path = ""
-        val expectedPath = ""
-        val expectedCommands = ""
-        val executor = StateExecutor(
-            FSM(S1,
+        val executor = TestStateExecutor(
+            FSM(A,
             FSMTable(
-                entry(S1, Start, S1) {
-                    path+="S1"
-                    commands += it
-                                     },
-                entry(S1, Help, S2) {
-                    path += "S2"
-                    commands += it
-                                    },
-                entry(S1, Text::class) { cmd ->
-                    (cmd.arguments.getOrNull()?.let {
-                        commands += "$cmd($it)"
-                        if (it == "1") {
-                            S1
+                entry(A, Start, B) {},
+                entry(A, Help, C) {},
+                entry(A, Back, A) {},
+                entry(A, Text::class) { cmd ->
+                    cmd.arguments.getOrNull()?.let {
+                        when (it) {
+                            "1" -> A
+                            "2" -> B
+                            "3" -> C
+                            else -> D
                         }
-                        if (it == "2"){
-                            S2
-                        }
-                        if (it == "3"){
-                            S3
-                        }
-                        else {
-                            S4
-                        }
-                    } ?: S4).also {
-                        path += it
-                    }
+                    } ?: D
                 },
 
-                entry(S2, Help, S1) {
-                    path += "S1"
-                    commands += it
-                                    },
-                entry(S2, Start, S3) {
-                    path += "S3"
-                    commands += it},
-                entry(S2, Back, S1) {
-                    path += "S1"
-                    commands += it},
-                entry(S2, Text::class, S1) {
-                    path += "S1"
-                    commands += it},
+                entry(B, Help, A) {},
+                entry(B, Start, C) {},
+                entry(B, Back, A) {},
+                entry(B, Text::class, A) {},
 
-                entry(S3, Help, S2) {
-                    path += "S2"
-                    commands += it},
-                entry(S3, Start, S1) {
-                    path += "S1"
-                    commands += it},
-                entry(S3, Back, S1) {
-                    path += "S1"
-                    commands += it},
-                entry(S3, Text::class, S2) {
-                    path += "S2"
-                    commands += it},
+                entry(C, Help, B) {},
+                entry(C, Start, A) {},
+                entry(C, Back, A) {},
+                entry(C, Text::class, B) {},
 
-                entry(S4, Command::class, S4) {
-                    path += "S4"
-                    commands += it
-                }
+                entry(D, Command::class, D) {}
                 )
         ))
 
+        executor.apply {
+            // A
+            submit(Start)// B
+            submit(Back)
+            submit(Back) // A
+            submit(Back)
+            submit(Back)
+            submit(Back) // A
 
+            submit(Help) // C
+            submit(Back) // A
+            submit(Help) // C
+            submit(Help) // B
+            submit(Start) // C
+            submit(Help) // B
+            submit(Start) // C
 
+            submit(Text("1")) // B
+            submit(Start) // C
+            submit(Text("2")) // B
+            submit(Start) // C
+            submit(Text("3")) // B
+            submit(Back) // A
+
+            submit(Text("1"))
+            submit(Back)
+            submit(Text("2"))
+            submit(Back)
+            submit(Text("3"))
+            submit(Back)
+
+            submit(Start) // B
+            submit(Text("Any")) // A
+            submit(Help) // C
+            submit(Text("Any")) // B
+            submit(Text("Any")) // A
+            submit(Text("Any")) // D
+
+            submit(Help)
+            submit(Start)
+            submit(Help)
+            submit(Start)
+            submit(Start)
+            submit(Text("Any"))
+            submit(Back)
+            submit(Text("Any"))
+            submit(Start)
+        }
+
+        val expectedCommands = "StartBackBackBackBackBack" +
+                "HelpBackHelpHelpStartHelpStart" +
+                "Text(1)StartText(2)StartText(3)Back" +
+                "Text(1)BackText(2)BackText(3)Back" +
+                "StartText(Any)HelpText(Any)Text(Any)Text(Any)" +
+                "HelpStartHelpStartStartText(Any)BackText(Any)Start"
+        val expectedPath = "ABAAAAA" +
+                "CACBCBC" +
+                "BCBCBA" +
+                "AABACA" +
+                "BACBAD" +
+                "DDDDDDDDD"
+        assertEquals(expectedCommands, executor.commands)
+        assertEquals(expectedPath, executor.path)
     }
 }
 
 class TestStateExecutor(fsm: FSM): StateExecutor(fsm){
 
-    private var path = ""
-    private var commands = ""
+    var path = fsm.start.javaClass.simpleName
+    var commands = ""
 
     override fun submit(submittedCommand: Command) {
-        commands += submittedCommand.javaClass.simpleName
+        commands += submittedCommand
         super.submit(submittedCommand)
-        path += current
+        path += current.javaClass.simpleName
     }
 }
 
-object S1: State
-object S2: State
-object S3: State
-object S4: State
+object A: State
+object B: State
+object C: State
+object D: State
 
 object Start: BaseCommand()
 object Help: BaseCommand()
