811476798f96cff96cf3259a1208977c041fdf25
Denys Chernyshov <den.blackshov@gmail.com>
2022-10-10 20:55:00 +0200

    refactor telegram menu navigator

--- main/src/main/kotlin/coden/alec/bot/menu/TelegramMenuNavigatorDirector.kt
+++ main/src/main/kotlin/coden/alec/bot/menu/TelegramMenuNavigatorDirector.kt
@@ -1,24 +1,31 @@
 package coden.alec.bot.menu
 
 import coden.alec.app.menu.MenuNavigatorFactory
+import coden.alec.bot.utils.edit
+import coden.alec.bot.utils.send
+import coden.alec.bot.view.TelegramMenuFormatter
+import coden.fsm.Command
+import com.github.kotlintelegrambot.Bot
+import com.github.kotlintelegrambot.entities.Message
 
 
-class TelegramMenuNavigatorDirector(private val factory: MenuNavigatorFactory)
-{
+class TelegramMenuNavigatorDirector(private val factory: MenuNavigatorFactory) {
     private val menus = HashMap<Long, TelegramMenuNavigator>()
+    private val formatter = TelegramMenuFormatter()
 
-    fun createNewMenu(bot: Bot, message: Message){
+    fun createNewMainMenu(bot: Bot, message: Message) {
         val controller = TelegramMenuNavigator(factory.mainMenuNavigator())
-        val (text, replyMarkup) = controller.createMain()
-        val id = bot.send(message, text, replyMarkup = replyMarkup).get().messageId
+        val response = formatter.format(controller.createMainMenu())
+        val id = bot.send(message, response.message, replyMarkup = response.replyMarkup).get().messageId
         menus[id] = controller
     }
 
-    fun handleCommand(bot: Bot, message: Message, data: String): Command?{
+    fun handleCommand(bot: Bot, message: Message, data: String): Command? {
         return menus[message.messageId]?.let {
-            val (text, markup, action) = it.navigate(data)
-            bot.edit(message, text=text, replyMarkup = markup)
-            return action
+            val navResult = it.navigate(data)
+            val result = formatter.format(navResult.menu)
+            bot.edit(message, text = result.message, replyMarkup = result.replyMarkup)
+            return navResult.action
         }
     }
 
