67cc98e9a7ccc230cfe61de13a47a2da20c66fac
Denys Chernyshov <den.blackshov@gmail.com>
2022-11-15 23:32:02 +0100

    ADd CreateScaleWithArgs test case

--- main/src/test/kotlin/coden/alec/main/table/ScaleTableTest.kt
+++ main/src/test/kotlin/coden/alec/main/table/ScaleTableTest.kt
@@ -1,8 +1,12 @@
 package coden.alec.main.table
 
 import coden.alec.app.actuators.ScaleActuator
+import coden.alec.app.actuators.scale.InvalidScaleFormatException
+import coden.alec.app.fsm.CreateScaleCommand
 import coden.alec.app.fsm.ListScalesCommand
 import coden.alec.app.fsm.Start
+import coden.alec.interactors.definer.scale.CreateScaleRequest
+import coden.alec.interactors.definer.scale.CreateScaleResponse
 import coden.alec.interactors.definer.scale.ListScalesResponse
 import coden.fsm.FSMVerifier
 import org.junit.jupiter.api.BeforeEach
@@ -10,8 +14,8 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
-import org.mockito.kotlin.any
 import org.mockito.kotlin.whenever
+import java.lang.IllegalStateException
 
 @ExtendWith(MockitoExtension::class)
 class ScaleTableTest{
@@ -28,13 +32,68 @@ class ScaleTableTest{
     @Test
     fun listScales() {
         val response = ListScalesResponse(emptyList())
-        whenever(actuator.listScales()).thenReturn(Result.success(response))
+        val error = IllegalStateException()
+
+        whenever(actuator.listScales())
+            .thenReturn(Result.success(response))
+            .thenReturn(Result.failure(error))
 
         verifier
             .submit(ListScalesCommand)
             .verify(actuator) {listScales()}
-            .verify(actuator) {respondListScales(any())}
+            .verify(actuator) {respondListScales(response)}
+            .verifyState(Start)
+
+            .submit(ListScalesCommand)
+            .verify(actuator) {listScales()}
+            .verify(actuator) {respondInternalError(error)}
             .verifyState(Start)
 
     }
+    @Test
+     fun createScalesWithArgs() {
+        val request = genRequest()
+        val response = genResponse()
+        val error = IllegalStateException()
+        val userError = InvalidScaleFormatException("")
+        whenever(actuator.parseCreateScaleRequest("scale"))
+            .thenReturn(Result.success(request)) // 0
+            .thenReturn(Result.failure(userError)) // 1
+            .thenReturn(Result.failure(error))  // 2
+            .thenReturn(Result.success(request)) // 3
+
+        whenever(actuator.createScale(request))
+            .thenReturn(Result.success(response)) // 0
+            .thenReturn(Result.failure(error))   // 3
+
+        verifier
+            .submit(CreateScaleCommand("scale"))
+            .verify(actuator) {parseCreateScaleRequest("scale")}
+            .verify(actuator) {createScale(request) }
+            .verify(actuator) {respondCreateScale(response)}
+            .verifyState(Start)
+
+            .submit(CreateScaleCommand("scale"))
+            .verify(actuator) {parseCreateScaleRequest("scale")}
+            .verify(actuator) {respondInvalidScaleFormat(userError)}
+            .verifyState(Start)
+//
+            .submit(CreateScaleCommand("scale"))
+            .verify(actuator) {parseCreateScaleRequest("scale")}
+            .verify(actuator) {respondInternalError(error)}
+            .verifyState(Start)
+
+            .submit(CreateScaleCommand("scale"))
+            .verify(actuator) {parseCreateScaleRequest("scale")}
+            .verify(actuator) {createScale(request)}
+            .verify(actuator) {respondInternalError(error)}
+            .verifyState(Start)
+
+    }
+
+    private fun genResponse() = CreateScaleResponse("scale-0")
+
+    private fun genRequest() = CreateScaleRequest(
+        "scale", "unit", mapOf(1L to "some")
+    )
 }
